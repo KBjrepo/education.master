bf68f7b49495410fbb73924f130726818cfec04e
KBjrepo <kamman@inbox.ru>
2019-02-07 16:52:42 +0300

    Добавил waitJS от Юры

--- src/main/groovy/lesson5/helpers/WebDriverHelper.groovy
+++ src/main/groovy/lesson5/helpers/WebDriverHelper.groovy
@@ -1,26 +1,26 @@
 package lesson5.helpers
 
+import com.codeborne.selenide.Selenide
 import org.awaitility.Awaitility
 import org.awaitility.Duration
 
-import java.util.concurrent.TimeUnit
-
-import static org.hamcrest.MatcherAssert.assertThat
-import static org.hamcrest.Matchers.equalTo
-
 class WebDriverHelper {
-    public waitJS() {
-        Awaitility.await().
-                atMost(new Duration(30, TimeUnit.SECONDS)). //таймаут в 30 секунд
-                pollInterval(Duration.FIVE_SECONDS). //проверяем каждые 5 секунд
-                pollInSameThread(). //проверяем в том же потоке, что и вызвали Awaitility.await()
-                ignoreExceptions(). //игнорируем все исключения, кроме AssertionError
-                untilAsserted({ //ждем, что ассерт не выкинет исключение AssertionError
-                    assertThat(
-                            "Неправильный поисковый запрос", //сообщение об ошибке в случае таймаута
-                            SearchSteps.getCurrentSearchRequest(), //вызов метода, который возвращает реальный результат
-                            equalTo("котики"))
-                    //инструкция по проверке - актуальный результат должен быть равен "котики"
-                })
+    static boolean isJQueryReady() {
+        try {
+            return Selenide.executeJavaScript("return jQuery.active==0")
+        } catch (Throwable ignored) {
+            return false
+        }
+    }
+
+    static waitJS() {
+        Awaitility.await()
+                .atMost(Duration.ONE_MINUTE)
+                .pollInterval(Duration.ONE_SECOND)
+                .pollInSameThread().ignoreExceptions().until(
+                {
+                    isJQueryReady()
+                }
+        )
     }
 }
